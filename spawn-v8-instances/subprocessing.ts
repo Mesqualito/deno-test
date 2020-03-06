2251f6e0395c111426395b133c7098f83bfd826f
Mesqualito <jochen@gebsattel.rocks>
2020-03-06 13:26:38 +0100

    Deno service workers. Test with '$ deno --allow-run subprocessing.ts'

---
+++ spawn-v8-instances/subprocessing.ts
@@ -0,0 +1,18 @@
+const p = Deno.run({
+    args: ["deno", "run", "--allow-read", "../promises/top_level_await.ts", "../promises/dummyFile.txt"],
+    stdout: "piped",
+    stderr: "piped"
+});
+
+const { code } = await p.status();
+
+if (code === 0) {
+    const rawOutput = await p.output();
+    await Deno.stdout.write(rawOutput);
+} else {
+    const rawError = await p.stderrOutput();
+    const errorString = new TextDecoder().decode(rawError);
+    console.log(errorString);
+}
+
+Deno.exit(code);
