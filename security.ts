c288b50e21d541a947f8db03ff000c469dc194af
Mesqualito <jochen@gebsattel.rocks>
2020-03-06 12:39:26 +0100

    Testing deno with imports

--- security.ts
+++ security.ts
@@ -1,9 +1,16 @@
-// has to be run with: '--allow-net' or '--allow-all' flag!
-
-console.info("Hello there!");
-
 import {serve} from "https://deno.land/std/http/server.ts";
+import {green} from "https://raw.githubusercontent.com/denoland/deno/master/std/fmt/colors.ts";
+import capitalize from "https://unpkg.com/lodash-es@4.17.15/capitalize.js";
 
 const server = serve(":8000");
 
-console.info("Server created!");
+console.info(green(capitalize("server created!")));
+
+const body = new TextEncoder().encode("Hello there\n");
+
+(async () => {
+    console.log(green("Listening on http://localhost:8000/"));
+    for await (const req of server) {
+        req.respond({ body });
+    }
+})();
